0e3da337caf0c8f936617000b38d6dea02d2d874
Nathan Victor Jean-Claude Brodin <nathan.victor.jean-claude.brodin@dnb.no>
2024-07-10 12:54:15 +0200

    imrpove SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,21 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Nathan's AI",
-  description: "To try out stuff",
+  description: "Nathan is here to help you with anything!",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
