029c1293513bf30224f557217676bde7b1c7c11c
Nathan Victor Jean-Claude Brodin <nathan.victor.jean-claude.brodin@dnb.no>
2024-07-11 13:56:25 +0200

    feat: new themes

--- registry/themes.ts
+++ registry/themes.ts
@@ -23,6 +23,30 @@ export const themes = [
       dark: "27.06 100% 80%",
     },
   },
+  {
+    name: "forest",
+    label: "Forest",
+    activeColor: {
+      light: "137 45% 65%",
+      dark: "137 45% 65%",
+    },
+  },
+  {
+    name: "chocolate",
+    label: "Chocolate",
+    activeColor: {
+      light: "36 45% 70%",
+      dark: "36 45% 70%",
+    },
+  },
+  {
+    name: "rain",
+    label: "Rain",
+    activeColor: {
+      light: "240 0% 90%",
+      dark: "240 0% 90%",
+    },
+  },
 ] as const;
 
 export type Theme = (typeof themes)[number];
