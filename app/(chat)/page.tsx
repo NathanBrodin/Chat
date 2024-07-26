465875179af7c41739e4d9a451fa1f7e5b488957
Nathan Victor Jean-Claude Brodin <nathan.victor.jean-claude.brodin@dnb.no>
2024-07-26 10:23:09 +0200

    refactor: moved sidebar

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,4 +1,5 @@
 import Chat from "@/components/chat"
+import { SideBar } from "@/components/side-bar"
 import { AI } from "@/lib/chat/actions"
 
 // Force the page to be dynamic and allow streaming responses up to 30 seconds
@@ -7,10 +8,13 @@ export const maxDuration = 30
 
 export default function Home() {
   return (
-    <main className="flex h-[100dvh] w-screen bg-background text-foreground">
+    <main className="relative flex h-[100dvh] w-screen bg-background text-foreground">
       <AI>
         <Chat />
       </AI>
+      <div className="absolute left-0 top-0 z-20 m-4">
+        <SideBar />
+      </div>
     </main>
   )
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -5,7 +5,6 @@ import Link from "next/link"
 import { Conversation } from "@/components/conversation"
 import { EmptyConversation } from "@/components/empty-conversation"
 import { PromptForm } from "@/components/prompt-form"
-import { SideBar } from "@/components/side-bar"
 import { Separator } from "@/components/ui/separator"
 import { useUIState } from "@/hooks/use-ai"
 
@@ -13,10 +12,9 @@ export default function Chat() {
   const [messages, setMessages] = useUIState()
 
   return (
-    <div className="relative flex h-full w-full flex-col items-center">
+    <div className="flex h-full w-full flex-col items-center">
       <AnimatePresence initial={false}>{messages.length === 0 && <EmptyConversation />}</AnimatePresence>
       <Conversation messages={messages} />
-
       <Separator />
       <div className="flex w-full justify-between p-1 sm:p-4">
         <Link
@@ -28,9 +26,6 @@ export default function Chat() {
         </Link>
         <PromptForm messages={messages} setMessages={setMessages} />
         <div />
-      </div>
-      <div className="absolute left-0 top-0 z-20 m-4">
-        <SideBar />
       </div>
     </div>
   )
