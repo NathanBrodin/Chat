11200d96c947fad58bbfbd0ce06684a93aeca1e7
Nathan Brodin <nathan@brodin.dev>
2024-08-04 10:18:27 +0200

    feat: getting user location

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,12 +1,21 @@
 import Chat from "@/components/chat"
 import { SideBar } from "@/components/side-bar"
 import { AI } from "@/lib/chat/actions"
+import { searchParamsToGeo } from "@/lib/utils"
 
 // Force the page to be dynamic and allow streaming responses up to 30 seconds
 export const dynamic = "force-dynamic"
 export const maxDuration = 30
 
-export default function Home() {
+type PageProps = {
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function Home({ searchParams }: PageProps) {
+  const location = searchParamsToGeo(searchParams)
+
+  console.log(location)
+
   return (
     <main className="relative flex h-[100dvh] w-screen bg-background text-foreground">
       <AI>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
+import { Geo } from "@vercel/edge"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+type searchParams = { [key: string]: string | string[] | undefined }
+
+export function searchParamsToGeo(searchParams: searchParams) {
+  const location: Geo = {
+    city: searchParams.city as string,
+    country: searchParams.country as string,
+    flag: searchParams.flag as string,
+    region: searchParams.region as string,
+    countryRegion: searchParams.countryRegion as string,
+    latitude: searchParams.latitude as string,
+    longitude: searchParams.longitude as string,
+  }
+  return location
 }
