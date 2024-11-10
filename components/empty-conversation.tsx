98700667483c9879c7260730168ca03271e85536
Nathan Brodin <nathan@brodin.dev>
2024-11-10 15:08:47 +0100

    feat: dazzle animation

--- components/empty-conversation.tsx
+++ components/empty-conversation.tsx
@@ -2,6 +2,7 @@ import { Geo } from "@vercel/edge"
 import { motion } from "framer-motion"
 import { ExampleQuestions } from "./example-questions"
 import { IconNathansAI } from "./ui/icons"
+import Dazzle from "./ui/dazzle"
 
 type EmptyConversationProps = {
   location: Geo
@@ -18,6 +19,7 @@ export function EmptyConversation({ location, addMessage }: EmptyConversationPro
       }}
       transition={{ ease: [0.32, 0.72, 0, 1], duration: 0.3 }}
     >
+      <Dazzle text="Nathan's " accent="AI." />
       <IconNathansAI className="size-12" />
       <h1 className="font-display text-5xl font-semibold">
         <span className="text-shadow-primary">Nathan&apos;s</span>
