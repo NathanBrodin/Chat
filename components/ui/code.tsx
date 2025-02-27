e5300ab1f5f28f62a8a1cc75487b02c063fd853e
Nathan Brodin <nathan@brodin.dev>
2025-02-27 14:08:30 +0100

    chore: upgrade packages to latest version

--- components/ui/code.tsx
+++ components/ui/code.tsx
@@ -8,7 +8,7 @@ export function CodeBlock({ ...props }) {
   const { resolvedTheme } = useTheme()
 
   // Inline code
-  if (!props.className?.includes("language" || "language")) {
+  if (!props.className?.includes("language")) {
     return <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">{props.children}</code>
   }
 
