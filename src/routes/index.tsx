6ab24f72b2e48419bc337bc01e598bf9d50240a8
Nathan Brodin <nathan@brodin.dev>
2026-04-07 13:53:53 +0200

    coss/ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { useMutation, useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { Authenticated, Unauthenticated, AuthLoading } from 'convex/react'
 
+import Particle from '@/components/test'
 import { authClient } from '@/lib/auth/auth-client'
 
 import { api } from '../../convex/_generated/api'
@@ -44,6 +45,7 @@ function App() {
       >
         Sign in with Email
       </button>
+      <Particle />
     </div>
   )
 }
