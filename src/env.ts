f2d1ce413395cf5019f9e76baadd5ca9e95eeaac
Nathan Brodin <nathan@brodin.dev>
2026-04-07 09:39:07 +0200

    initial setup

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    CONVEX_DEPLOYMENT: z.string(),
+    BETTER_AUTH_URL: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+  },
+  clientPrefix: "VITE_",
+  client: {
+    VITE_CONVEX_URL: z.string(),
+  },
+  runtimeEnv: import.meta.env,
+  emptyStringAsUndefined: true,
+});
