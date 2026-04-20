bc54cb99d014e1a64f2dc39be8411158c3faadd3
Nathan Brodin <nathan@brodin.dev>
2026-04-20 20:49:36 +0200

    add reasonning part

--- src/components/chat/conversation.tsx
+++ src/components/chat/conversation.tsx
@@ -6,9 +6,9 @@ import {
   ConversationEmptyState,
   ConversationScrollButton,
 } from '@/components/ai-elements/conversation'
-import { Message, MessageContent, MessageResponse } from '@/components/ai-elements/message'
 
 import { useChatContext } from '.'
+import { ChatMessage } from './message'
 
 export function ChatConversation() {
   const { messages, status } = useChatContext()
@@ -23,27 +23,12 @@ export function ChatConversation() {
             description="Type a message below to begin chatting"
           />
         ) : (
-          messages.map((message) => (
-            <Message from={message.role} key={message.id}>
-              <MessageContent>
-                {message.parts.map((part, i) => {
-                  switch (part.type) {
-                    case 'text':
-                      return (
-                        <MessageResponse
-                          key={`${message.id}-${i}`}
-                          isAnimating={status === 'streaming' && message.role === 'assistant'}
-                          animated={{ animation: 'blurIn' }}
-                        >
-                          {part.text}
-                        </MessageResponse>
-                      )
-                    default:
-                      return null
-                  }
-                })}
-              </MessageContent>
-            </Message>
+          messages.map((message, index) => (
+            <ChatMessage
+              message={message}
+              isLastMessage={index === messages.length - 1}
+              isStreaming={status === 'streaming'}
+            />
           ))
         )}
       </ConversationContent>
